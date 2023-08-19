783993ab6d0d6e066d7d699fd41073d83e186540
Reyyahs <133382184+Reyyahs@users.noreply.github.com>
2023-08-19 16:14:21 -0400

    added models

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [
+      /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+      'Please enter a valid email address',
+    ],
+  },
+  thoughts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Thought' }],
+  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+}, {
+  toJSON: {
+    virtuals: true,
+  },
+  id: false,
+});
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;

11574d78ce95cb947f6c2718deb74729322f22eb
Reyyahs <133382184+Reyyahs@users.noreply.github.com>
2023-08-19 16:23:11 -0400

    added thought routes

---
+++ api/thoughtRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const thoughtController = require('../../controllers/thoughtController');
+
+router.get('/', thoughtController.getAllThoughts);
+router.post('/', thoughtController.createThought);
+
+router.get('/:thoughtId', thoughtController.getSingleThought);
+router.put('/:thoughtId', thoughtController.updateThought);
+router.delete('/:thoughtId', thoughtController.deleteThought);
+
+router.post('/:thoughtId/reactions', thoughtController.createReaction);
+router.delete('/:thoughtId/reactions/:reactionId', thoughtController.removeReaction);
+
+module.exports = router;
